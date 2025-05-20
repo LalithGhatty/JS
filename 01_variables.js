e88ee68c7ccad92beab1fce8b0ead84ff5a34206
LalithGhatty <gvsklalith2005@gmail.com>
2025-05-20 16:35:50 +0530

    Add Js basic files

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 245654;
+let accountEmail = "gvsklalith2005@gmail.com";
+var accountPassword = "9999";
+accountCity = "Vijayawada";
+let accountState;
+// accountId = 2;
+/*Prefer not to use var because of scope issues in block and functional scope*/
+accountEmail  = "saikrishna.22bce20329@vitapstudent.ac.in";
+accountPassword = "4564";
+accountCity = "Hyderabad";
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState]);
+
+
