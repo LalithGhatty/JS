7e49b47d1102d48e7ed7cd3b94e1d5fbc1b300c2
LalithGhatty <gvsklalith2005@gmail.com>
2025-05-21 16:30:33 +0530

    Datatype conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = true
+console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+ let loggedIn = true;
+ console.log(loggedIn);
+ 
+ let no = Number(loggedIn);
+ console.log(no);
+ console.log(typeof no);
+ let no1 = 1;
+ console.log(no1);
+
+ let boolean_Value = Boolean(no1);
+ console.log(boolean_Value);
+
+let name  = "Lalith";
+console.log(name);
+
+console.log(typeof name);
+let bval = Boolean(name);
+console.log(bval);
+console.log(typeof bval);
+
+
+ 
+ 
+ // "33" => 33
+ // "33abc" => Nan type is Number only
