483e934137b72ab04fc636db23f35a10f4f1cbcc
LalithGhatty <gvsklalith2005@gmail.com>
2025-05-21 17:54:57 +0530

    Comparison of datatyes

---
+++ 04_comparison.js
@@ -0,0 +1,28 @@
+console.log( 2 > 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(null != 0);
+
+//there is a difference between equality and comparison operatiors in js.Comparison operators convert null to 0 and they compare whereas eqaulity doesnt do that
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+
+
+
+
+
+
