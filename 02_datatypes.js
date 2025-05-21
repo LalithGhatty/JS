558cf25e850c791e9ccd0ef06d26a13e47b7b61c
LalithGhatty <gvsklalith2005@gmail.com>
2025-05-21 12:21:13 +0530

    Datatypes file

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+ // alert("Hello"); //we are using node js not browser
+ console.log(3+3) ;console.log("Lalith");
+ let name = "Lalith";
+ let  no = 2;
+ let loggedIn  = false;
+ //number  => //2 to power 53
+ //bigint
+ //String => ""
+//boolean => true or false
+// null => standalone value
+//undefined =>
+//symbol +> unique
+console.log(typeof "Lalith"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
