04dadc5fb095904368d47120367361127b823765
LalithGhatty <gvsklalith2005@gmail.com>
2025-05-25 19:10:30 +0530

    Added strings topic

---
+++ 05_strings.js
@@ -0,0 +1,15 @@
+const name = "Lalith";
+const repoCount = 50;
+console.log(name+repoCount+"Value");
+console.log(`My name is ${name.toUpperCase()} and my repoCount is ${repoCount}`);
+
+const gameName = new String("LalithGhatty");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+
+
